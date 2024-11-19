d88bb8bc969e2f8818c8be2c3cb92ce1ae582f28
Annie Wang <annie.xd.wang@gmail.com>
2024-11-18 21:24:12 -0800

    update ome-tiff uuid

--- src/navigate/model/metadata_sources/ome_tiff_metadata.py
+++ src/navigate/model/metadata_sources/ome_tiff_metadata.py
@@ -218,7 +218,7 @@ class OMETIFFMetadata(XMLMetadata):
         # misc info
         ome_dict["StructuredAnnotations"] = {
             "MapAnnotation": {
-                "ID": "Misc-annotation",
+                "ID": "misc:annotation",
                 "Value": {
                     "Map": {
                         "M": [
